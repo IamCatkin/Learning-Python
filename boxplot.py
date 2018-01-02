968045963d7c38cc5170b40c3e572c2b6269d43d
unknown <>
2018-01-02 09:56:19 +0800

    增加保存

--- boxplot.py
+++ boxplot.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt  
+from matplotlib.backends.backend_pdf import PdfPages
 
 #添加成绩表
 plt.style.use("classic")
@@ -18,7 +19,8 @@ while seq < 23:
     seq += 1
 
 #用matplotlib来画出箱型图
-acc = plt.boxplot(x=df.values,
+acc = plt.figure() 
+plt.boxplot(x=df.values,
             patch_artist=True,
             boxprops = {'color':'black','facecolor':'#9999ff'},
             labels=df.columns,
@@ -27,3 +29,7 @@ plt.title('The Accuracy boxplot')
 plt.ylabel('Accuracy')
 plt.xlabel('Hepatotoxicity class')
 plt.show()
+
+#保存图片
+acc.savefig(".\\results\\acc.pdf", bbox_inches='tight')
+                       
